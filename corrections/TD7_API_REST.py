6d64da0fdacd3d6c2f97463da4e9b1db65a9b620
Romain Tavenard <romain.tavenard@univ-rennes2.fr>
2023-11-21 13:07:03 +0100

    update corrigé

--- corrections/TD7_API_REST.py
+++ corrections/TD7_API_REST.py
@@ -12,18 +12,29 @@ def api_position_bus(url):
     contenu_json = contenu_brut.json()
     return contenu_json
 
-def nb_bus_en_avance(les_bus):
-    nombre_bus_en_avance = 0
-    for bus in les_bus['records']:
-        if bus["fields"]["ecartsecondes"] > 0:
+def bus_en_service():
+    url = "https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-vehicules-position-tr&q=&rows=-1&refine=etat:En+ligne"
+    les_bus = api_position_bus(url)
+    liste_pos = []
+    for bus in les_bus["records"]:
+        d = {}
+        for cle in ["numerobus", "nomcourtligne", "destination", "ecartsecondes", "position"]:
+            d[cle] = bus[cle]
+        liste_pos.append(d)
+    return liste_pos
+
+def nb_bus_en_avance():
+    les_bus_en_service = bus_en_ligne()
+    for bus in les_bus_en_service:
+        if bus["ecartsecondes"] > 0:
             nombre_bus_en_avance+=1
     return nombre_bus_en_avance
 
 def ligne_de_bus_ecartensecondes(les_bus):
     dico_ligne_de_bus_ecartensecondes = {}
-    for bus in les_bus['records']:
-        nomcourtligne = bus["fields"]["nomcourtligne"]
-        ecartensecondes = bus["fields"]["ecartsecondes"]
+    for bus in bus_en_ligne():
+        nomcourtligne = bus["nomcourtligne"]
+        ecartensecondes = bus["ecartsecondes"]
         if nomcourtligne not in dico_ligne_de_bus_ecartensecondes.keys():
             dico_ligne_de_bus_ecartensecondes[nomcourtligne] = [ecartensecondes]
         else:
@@ -36,21 +47,25 @@ def lecture_fichier_json(nom_fic):
 
 def recherche_bus(les_bus, numero_bus):
     for bus in les_bus['records']:
-        if bus["fields"]["numerobus"] == numero_bus:
+        if bus["numerobus"] == numero_bus:
             return bus
 
 def adresse_bus(numero_bus, client_graphhopper):
-    url = "https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-vehicules-position-tr&q=&rows=-1&facet=numerobus&facet=nomcourtligne&facet=destination&facet=ecartsecondes&refine.etat=En+ligne"
+    url = "https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-vehicules-position-tr&q=&rows=-1&refine=etat:En+ligne"
     bus_en_ligne = api_position_bus(url)
     bus = recherche_bus(bus_en_ligne, numero_bus)
-    coord = bus["geometry"]["coordinates"]
-    return client_graphhopper.latlong_to_address(coord[::-1])
+    coord = bus["coordinates"]
+    return client_graphhopper.latlong_to_address(
+            coord["lat"], coord["lon"]
+        )
 
 
 # Tests
-url = "https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-vehicules-position-tr&q=&rows=-1&facet=numerobus&facet=nomcourtligne&facet=destination&facet=ecartsecondes&refine.etat=En+ligne"
+url = "https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-vehicules-position-tr&q=&rows=-1&refine=etat:En+ligne"
 bus_en_ligne = api_position_bus(url)
-#pprint(bus_en_ligne)
+# pprint(bus_en_ligne)
+print(len(bus_en_ligne["records"]))
+print(bus_en_ligne["total_count"])
 
 print(nb_bus_en_avance(bus_en_ligne))
 pprint(ligne_de_bus_ecartensecondes(bus_en_ligne))
